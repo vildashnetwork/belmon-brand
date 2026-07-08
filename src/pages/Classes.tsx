206972ec176d26eb327bac34cd4aa612b0b268e4
vildashetwork <vildashnetwork02@gmail.com>
2026-07-08 21:14:59 +0100

    corected

--- src/pages/Classes.tsx
+++ src/pages/Classes.tsx
@@ -676,7 +676,7 @@ function ClassDialog({
     setForm((f) => ({ ...f, [k]: v }));
 
   const classNames = ["Form 1", "Form 2", "Form 3", "Form 4", "Form 5", "Lower 6th", "Upper 6th", "Graduated"];
-  const departments = ["Science", "Arts", "Commercial"];
+  const departments = ["Science", "Arts", "Commercial", "General", "Technical"];
   const cycles = ["1st Cycle", "2nd Cycle"];
   const academicYears = ["2026-2027", "2027-2028", "2028-2029"];
 
